9d7ba386a4acfcc3122e454394ecbbd12e8158ae
Emine İnan <emineinanx@gmail.com>
2021-02-18 15:15:21 +0300

    Add sample app using Firebase Authentication

---
+++ app/src/main/java/com/example/firebaseauthapp/LoginActivity.kt
@@ -0,0 +1,39 @@
+package com.example.firebaseauthapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var  auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth= FirebaseAuth.getInstance()
+    }
+
+    fun login(view: View){
+        val email=editTextEmailAddress.text.toString()
+        val password=editTextPassword.text.toString()
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                val intent= Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun goToRegister(view: View){
+        val intent= Intent(this,RegisterActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/firebaseauthapp/RegisterActivity.kt
@@ -0,0 +1,41 @@
+package com.example.firebaseauthapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var  auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        auth= FirebaseAuth.getInstance()
+    }
+
+    fun register(view: View){
+        val email=editTextEmailAddress.text.toString()
+        val password=editTextPassword.text.toString()
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                val intent= Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun goToLogin(view: View){
+        val intent= Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+}
